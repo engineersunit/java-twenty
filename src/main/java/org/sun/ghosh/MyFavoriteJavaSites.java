3ac06e2947c512c0ff51178fbd15a21b173567a5
Sunit Ghosh <engineersunit@gmail.com>
2023-04-04 21:52:21 +0530

    Added cases and code for Java 20 Feature - Structured Concurrency

--- src/main/java/org/sun/ghosh/MyFavoriteJavaSites.java
+++ src/main/java/org/sun/ghosh/MyFavoriteJavaSites.java
@@ -1,5 +1,7 @@
 package org.sun.ghosh;
 
+import jdk.incubator.concurrent.StructuredTaskScope;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -9,69 +11,120 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 public class MyFavoriteJavaSites {
     enum Mode {
-        VIRTUAL,
-        PLATFORM
+        VIRTUAL, PLATFORM, QUICK, // StructuredTaskScope.ShutdownOnSuccess
+        ALL_OR_NONE // StructuredTaskScope.ShutdownOnFailure
     }
 
     public static void main(String[] args) {
         String mode = args[0];
-        Mode runMode = Mode.valueOf(mode);
-        List<String> myFavSitesList = readFileInList("D:\\MyFavSites.txt");
+        Mode runMode = Mode.valueOf(Objects.isNull(mode) ? "VIRTUAL" : mode);
         List<Future> futures = new ArrayList<>();
-        long startTime = System.nanoTime();
-        for (String favUrl : myFavSitesList) {
-            URL url;
-            try {
-                url = new URI(favUrl).toURL();
-            } catch (URISyntaxException | MalformedURLException e) {
-                throw new RuntimeException(e);
-            }
 
-            switch (runMode) {
-                case VIRTUAL:
-                    try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
-                        Future future1 = executor.submit(() -> fetchURL(url));
-                        futures.add(future1);
+        List<URL> myFavSitesURLList = getURLsFromSource();
 
-                    }
-                    break;
-                case PLATFORM:
-                    try (var executor = Executors.newCachedThreadPool()) {
-                        Future future1 = executor.submit(() -> fetchURL(url));
-                        futures.add(future1);
-                    }
-            }
+
+        Instant startTime = Instant.now();
+
+        switch (runMode) {
+            case VIRTUAL:
+                try (var executor =
+                             Executors.newVirtualThreadPerTaskExecutor()) {
+                    futures =
+                            myFavSitesURLList.stream()
+                                    .map(url -> executor.submit(() -> fetchURL(url)))
+                                    .collect(Collectors.toList());
+
+                }
+                break;
+            case PLATFORM:
+                try (var executor = Executors.newCachedThreadPool()) {
+                    futures =
+                            myFavSitesURLList.stream()
+                                    .map(url -> executor.submit(() -> fetchURL(url)))
+                                    .collect(Collectors.toList());
+                }
+                break;
+            case QUICK:
+                try (var scope =
+                             new StructuredTaskScope.ShutdownOnSuccess<String>()) {
+                    futures =
+                            myFavSitesURLList.stream()
+                                    .map(url -> scope.fork(() -> fetchURL(url)))
+                                    .collect(Collectors.toList());
+
+                    var result = scope.join().result();
+
+                } catch (ExecutionException | InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            case ALL_OR_NONE:
+                try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
+                    futures =
+                            myFavSitesURLList.stream()
+                                    .map(url -> scope.fork(() -> fetchURL(url)))
+                                    .collect(Collectors.toList());
+                    scope.join();           // Join both forks
+                    scope.throwIfFailed(e -> e);  // ... and propagate errors
+                } catch (Throwable e) {
+                    throw new RuntimeException(e);
+                }
+                break;
         }
+
+
+        processFutures(futures);
+
+        Instant endTime = Instant.now();
+        Duration between = Duration.between(startTime, endTime);
+        System.out.println(String.format("Mode: %s. Created automated " +
+                        "reading" + " " + "digest " + "for: %d websites in %d" +
+                        "seconds or %d nanoseconds.",
+                runMode,
+                myFavSitesURLList.size(), between.get(ChronoUnit.SECONDS), between.get(ChronoUnit.NANOS)));
+
+    }
+
+    private static void processFutures(List<Future> futures) {
         // Remove from next iteration if URL is visited and written to file.
-        List<Future> futuresClone = (List<Future>) ((ArrayList<Future>) futures).clone();
+        List<Future> futuresClone =
+                (List<Future>) ((ArrayList<Future>) futures).clone();
         try (FileWriter fw = new FileWriter("D:\\MyFavSites.html")) {
-            while (!futuresClone.isEmpty())
+            while (!futuresClone.isEmpty()) {
                 for (Future f : futures) {
                     if (f.isDone()) {
-                        fw.write((String) f.resultNow());
-                        // Remove from next iteration if URL is visited and written to file.
+                        switch (f.state()) {
+                            case RUNNING, FAILED, CANCELLED -> {
+                            }
+                            case SUCCESS -> fw.write((String) f.resultNow());
+                        }
+                        // Remove from next iteration if URL is visited and
+                        // written to file.
                         futuresClone.remove(f);
                     } else {
                         continue;
                     }
                 }
+            }
             // Remove from next iteration if URL is visited and written to file.
             futures = (List<Future>) ((ArrayList<Future>) futuresClone).clone();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        long endTime = System.nanoTime();
-        System.out.println(String.format("Created automated reading digest for: %d websites in %d nanoseconds -> %d seconds %d milliseconds", myFavSitesList.size(), (endTime - startTime), (endTime - startTime) / 1000_000_000, ((endTime - startTime) / 1000_000) % 1000));
-
     }
 
     static String fetchURL(URL url) throws IOException {
@@ -80,15 +133,28 @@ public class MyFavoriteJavaSites {
         }
     }
 
-    public static List<String> readFileInList(String fileName) {
+    private static List<String> readFileInList(String fileName) {
 
         List<String> lines = Collections.emptyList();
         try {
-            lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+            lines = Files.readAllLines(Paths.get(fileName),
+                    StandardCharsets.UTF_8);
         } catch (IOException e) {
 
             throw new RuntimeException(e);
         }
         return lines;
+    }
+
+    private static List<URL> getURLsFromSource() {
+        return readFileInList("D:\\MyFavSites.txt").stream().map(url -> {
+            try {
+                return new URI(url).toURL();
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+        }).collect(Collectors.toList());
     }
 }
--- src/main/java/run.bat
+++ src/main/java/run.bat
@@ -1,3 +1,5 @@
-javac --enable-preview --release 20  -Xlint:preview org/sun/ghosh/MyFavoriteJavaSites.java
-java --enable-preview org.sun.ghosh.MyFavoriteJavaSites PLATFORM
-java --enable-preview org.sun.ghosh.MyFavoriteJavaSites VIRTUAL
+javac --enable-preview --release 20  -Xlint:preview --add-modules jdk.incubator.concurrent org/sun/ghosh/MyFavoriteJavaSites.java
+java --enable-preview --add-modules jdk.incubator.concurrent org.sun.ghosh.MyFavoriteJavaSites PLATFORM
+java --enable-preview --add-modules jdk.incubator.concurrent org.sun.ghosh.MyFavoriteJavaSites VIRTUAL
+java --enable-preview --add-modules jdk.incubator.concurrent org.sun.ghosh.MyFavoriteJavaSites QUICK
+java --enable-preview --add-modules jdk.incubator.concurrent org.sun.ghosh.MyFavoriteJavaSites ALL_OR_NONE
