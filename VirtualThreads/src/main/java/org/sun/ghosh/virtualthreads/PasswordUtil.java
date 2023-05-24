a519a9f4d82852b68c54c4b2d993f58257ba07f3
Sunit Ghosh <engineersunit@gmail.com>
2023-05-24 21:56:23 +0530

    Generating Password using Virtual Threads with CyclicBarrier injecting special characters

--- VirtualThreads/src/main/java/org/sun/ghosh/virtualthreads/PasswordUtil.java
+++ VirtualThreads/src/main/java/org/sun/ghosh/virtualthreads/PasswordUtil.java
@@ -6,7 +6,10 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -14,7 +17,11 @@ public class PasswordUtil {
 
     private final static List<Character> listOfPasswordCandidates = Collections.
             synchronizedList(new ArrayList<>());
-    final static int TYPE_MAX_CHAR = 2;
+    final static int TYPE_MAX_CHAR = 40000;
+
+    private static final CyclicBarrier cyclicBarrier = new CyclicBarrier(
+            2,
+            PasswordUtil::injectSpecialCharacter);
 
     public static void main(String[] args) {
         Instant startTime = Instant.now();
@@ -26,19 +33,39 @@ public class PasswordUtil {
             IntStream.rangeClosed(1, TYPE_MAX_CHAR).
                     forEach((i) ->
                             executor.submit(PasswordUtil::generateData));
+
+            // Disable new tasks from being submitted
+            executor.shutdown();
+            try {
+                // Wait a while for existing tasks to terminate
+                if (!executor.awaitTermination(1, TimeUnit.SECONDS)) {
+                    executor.shutdownNow(); // Cancel currently executing tasks
+                    // Wait a while for tasks to respond to being cancelled
+                    if (!executor.awaitTermination(2, TimeUnit.SECONDS))
+                        System.err.println("Created Virtual Thread Per Task Executor did not terminate");
+                } else {
+                    System.out.println("Created Virtual Thread Per Task Executor terminated");
+                }
+            } catch (InterruptedException ie) {
+                // (Re-)Cancel if current thread also interrupted
+                executor.shutdownNow();
+                // Preserve interrupt status
+                Thread.currentThread().interrupt();
+            }
         }
 
-        System.out.println("Use this password:\n"+
-                listOfPasswordCandidates
+        String password = listOfPasswordCandidates
                 .stream()
                 .map(Object::toString)
-                .collect(Collectors.joining()));
+                .collect(Collectors.joining());
+
+        System.out.println("Use this password:\n" + password);
 
         Instant endTime = Instant.now();
         Duration between = Duration.between(startTime, endTime);
         System.out.printf("Created password of length %d in %d seconds and %f milliseconds.",
-                TYPE_MAX_CHAR*4, between.get(ChronoUnit.SECONDS),
-                between.get(ChronoUnit.NANOS)/1000_000.0);
+                password.length(), between.get(ChronoUnit.SECONDS),
+                between.get(ChronoUnit.NANOS) / 1000_000.0);
     }
 
     /**
@@ -47,7 +74,7 @@ public class PasswordUtil {
      * 97-122 - a-z
      *
      * <a href="https://owasp.org/www-community/password-special-characters">Password Special Characters</a>
-     *
+     * <p>
      * 33	!	Exclamation
      * 35	#	Number sign (hash)
      * 36	$	Dollar sign
@@ -57,19 +84,33 @@ public class PasswordUtil {
      * 64	@	At sign
      * 126	~	Tilde
      */
-    private static void generateData(){
-        // 65-90  - A-Z
-        listOfPasswordCandidates.
-                add(generateRandomChar(90, 65));
+    private static void generateData() {
+
         // 97-122 - a-z
         listOfPasswordCandidates.
                 add(generateRandomChar(122, 97));
+
         // 48-57  - 0-9
         listOfPasswordCandidates.
                 add(generateRandomChar(57, 48));
+
+        // 65-90  - A-Z
+        listOfPasswordCandidates.
+                add(generateRandomChar(90, 65));
+
+        try {
+            cyclicBarrier.await();
+        } catch (InterruptedException |
+                 BrokenBarrierException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void injectSpecialCharacter() {
         listOfPasswordCandidates.
                 add(generateRandomSpecialChar());
     }
+
     private static char generateRandomChar(int max, int min) {
         return (char) (Math.random() * (max - min + 1) + min);
     }
